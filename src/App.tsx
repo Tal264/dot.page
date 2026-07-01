70646e20beac267d888a94e1fd319561dcd1ff59
keren binder <talbinder1@gmail.com>
2026-07-01 19:45:55 +0300

    fixed whatsapp message

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const queryClient = new QueryClient();
 /* WhatsApp Floating Widget */
 const WhatsAppWidget = () => {
   const message = encodeURIComponent(
-    "שלום, הגעתי דרך האתר שלכם ואשמח לקבל ייעוץ בחינם"
+    "היי! הגעתי דרך האתר שלכם ואשמח לקבל מידע נוסף על השירותים שלכם"
   );
 
   return (
